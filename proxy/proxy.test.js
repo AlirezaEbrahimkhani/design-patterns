8947d441eaec605cf4644965de5b507209156b85
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2022-01-17 20:05:23 +0330

    [Feat] [Test] test proxy pattern

---
+++ proxy/proxy.test.js
@@ -0,0 +1,46 @@
+const { it, expect, describe } = require("@jest/globals");
+const {
+  Student,
+  withReflectStudent,
+  withoutReflectStudent,
+} = require("./proxy");
+
+describe("Proxy pattern test", () => {
+  it("should has property (student obj)", () => {
+    expect(Student).not.toBeNull();
+  });
+
+  it("withoutReflect: should change the name property", () => {
+    withoutReflectStudent.name = "Alireza";
+    expect(withoutReflectStudent.name).toBe("Student name is Alireza");
+  });
+
+  it("withReflect: should change the name property", () => {
+    withReflectStudent.name = "Alireza";
+    expect(withReflectStudent.name).toBe("Student name is Alireza");
+  });
+
+  it("withReflect: should throw error while assign invalid type value", () => {
+    expect(() => {
+      withReflectStudent.name = 2;
+    }).toThrow(TypeError);
+  });
+
+  it("withoutReflect: should throw error while assign invalid type value", () => {
+    expect(() => {
+      withoutReflectStudent.name = 2;
+    }).toThrow(TypeError);
+  });
+
+  it("withReflect: should throw a correct error message while assign invalid type value", () => {
+    expect(() => {
+      withReflectStudent.name = 2;
+    }).toThrow("Type of values is not the same.");
+  });
+
+  it("withoutReflect: should throw a correct error message while assign invalid type value", () => {
+    expect(() => {
+      withoutReflectStudent.name = 2;
+    }).toThrow("Type of values is not the same.");
+  });
+});
